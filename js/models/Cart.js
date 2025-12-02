7e2ff4bcbd3474564b9ef25364d8293e006f7b55
Matheus Reboucas <reboucas444@gmail.com>
2025-12-02 15:06:59 -0300

    feat: add Product and Cart models

---
+++ js/models/Cart.js
@@ -0,0 +1,43 @@
+export class Cart {
+  constructor() {
+    this.items = []
+  }
+
+  addProduct(product) {
+    const existingItem = thiss.items.find(
+      item => item.product.id === product.id,
+    )
+
+    if (existingItem) {
+      existingItem.quantity++
+    } else {
+      this.items.push({ product, quantity: 1 })
+    }
+  }
+
+  updateQuantity(productId, quantity) {
+    const item = this.items.find(item.product.id === productId)
+
+    if (!item) return
+
+    if (quantity <= 0 || Number.isNaN(quantity)) {
+      this.removeProduct(productId)
+    } else {
+      item.quantity = quantity
+    }
+  }
+
+  removeProduct(productId) {
+    this.items = this.items.filter(item => item.product.id !== productId)
+  }
+
+  getItems() {
+    return this.items
+  }
+
+  getTotal() {
+    return this.items.reduce((sum, item) => {
+      return sum + item.product.price * quantity
+    }, 0)
+  }
+}
